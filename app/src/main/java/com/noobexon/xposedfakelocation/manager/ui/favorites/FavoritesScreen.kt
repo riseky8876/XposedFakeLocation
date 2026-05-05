351fa739ca0f56945fcd4313d89fa513d7a54bcb
riseky8876 <108766275+riseky8876@users.noreply.github.com>
2026-05-05 18:55:25 +0700

    Update FavoritesScreen.kt

--- app/src/main/java/com/noobexon/xposedfakelocation/manager/ui/favorites/FavoritesScreen.kt
+++ app/src/main/java/com/noobexon/xposedfakelocation/manager/ui/favorites/FavoritesScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.*
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.noobexon.xposedfakelocation.data.model.FavoriteLocation
